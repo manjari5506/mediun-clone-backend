76bf51abab0cc97b152dc56e7139a3005730b961
Manjari <raikwar.manjari@gmail.com>
2023-09-09 21:54:51 +0530

    backend files created

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const url = process.env.MONGO_URL;
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
